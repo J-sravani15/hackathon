3f7c383bb7f25fce01689a21f87738ce654083c4
J-sravani15 <sravanijagarlamudi2@gmail.com>
2026-03-06 00:13:53 +0530

    Connection is completed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import predict
 
-app = FastAPI(title="Hackathon AI System")
+app = FastAPI()
+
+# CORS CONFIGURATION
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(predict.router)
 
