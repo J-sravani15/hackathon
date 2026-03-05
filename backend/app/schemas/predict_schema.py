9fe4e7afb8cfc86e79c2100f9ea92917dfbf2ec6
Yarnagula Pooja <poojay0231@gmail.com>
2026-03-05 18:10:08 +0530

    updated predictschema

--- backend/app/schemas/predict_schema.py
+++ backend/app/schemas/predict_schema.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
-class PredictionRequest(BaseModel):
-    text: str
+class QuoteInput(BaseModel):
 
-class PredictionResponse(BaseModel):
-    prediction: int
-    confidence: float
+    Driver_Age: int
+    Driving_Exp: int
+    Prev_Accidents: int
+    Prev_Citations: int
+    Coverage: int
+    Veh_Usage: int
+    Annual_Miles_Range: int
+    Vehicl_Cost_Range: int
+    Sal_Range: int
+    Quoted_Premium: float
